b3a84885b2829654b3facc345cf2e52e60ca38c7
abellajoiasim-maker <abellajoiasim@gmail.com>
2026-01-16 19:01:30 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,23 @@
+const CACHE_NAME = 'abella-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/cart_checkout_premium.html',
+  '/categoria.html',
+  '/galvanicas.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
 self.addEventListener('install', e=>{
-  console.log('Service Worker instalado');
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
+
 self.addEventListener('fetch', e=>{
-  e.respondWith(fetch(e.request));
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
 });
